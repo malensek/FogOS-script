506bbb4aca31fc9ce8656ea1d4923680a334c3cc
Matthew Malensek <matt@malensek.net>
2024-09-16 11:13:07 -0700

    Add shell scripting support

---
+++ tests/shell-scripting/test.sh
@@ -0,0 +1,10 @@
+#!/sh
+# Please add this to Makefile to test
+
+echo Testing shell script functionality
+echo 
+echo Welcome to my script
+echo My script is amazing
+echo Look at these files
+ls /
+echo Byte now
